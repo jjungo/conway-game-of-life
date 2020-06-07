78de0b12f0ce37a384b70a336542c80856ab53f8
Joel <j.jungo@gmail.com>
2020-06-07 13:24:24 +0200

    add test source skeleton

---
+++ tests/game-of-life-tests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by joel on 07/06/2020.
+//
+
+
+#include <catch2/catch.hpp>
+#include "game_of_life.hpp"
+
+TEST_CASE("empty test") {}
---
+++ tests/test_main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by joel on 31/05/2020.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
