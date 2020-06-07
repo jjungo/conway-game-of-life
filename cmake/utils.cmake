a461db0aa83128073a36b3b7ab15556e98b6066c
Joel <j.jungo@gmail.com>
2020-06-07 12:54:03 +0200

    add find_catch2 cmake utility

---
+++ cmake/utils.cmake
@@ -0,0 +1,25 @@
+
+function(find_catch2)
+	set(Catch2_FOUND Catch2-NOTFOUND PARENT_SCOPE)
+	set(catch_cmake_lists ${PROJECT_SOURCE_DIR}/external/Catch2/CMakeLists.txt)
+	if (NOT EXISTS ${catch_cmake_lists})
+		find_package(Git)
+		if (NOT Git_FOUND)
+			return()
+		endif()
+		execute_process(
+				COMMAND ${GIT_EXECUTABLE} submodule update --init -- Catch2
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/external
+				RESULT_VARIABLE git_submodule_error
+				ERROR_QUIET)
+		if (git_submodule_error OR NOT EXISTS ${catch_cmake_lists})
+			return()
+		endif()
+	endif()
+	add_subdirectory(external/Catch2)
+	list(APPEND CMAKE_MODULE_PATH
+			${PROJECT_SOURCE_DIR}/external/Catch2/contrib)
+	set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
+	set(Catch2_FOUND 1 PARENT_SCOPE)
+
+endfunction()
